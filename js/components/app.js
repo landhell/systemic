03c9660e7f41fbe25c080c9fcd79c9a34ba61668
kfng <landhell@gmail.com>
2026-02-19 22:06:34 +0800

    vue components

---
+++ js/components/app.js
@@ -0,0 +1,7 @@
+import Header from "./header.js"
+
+export default {
+  components: {
+    "header-component": Header
+  }
+}
---
+++ js/components/header.js
@@ -0,0 +1,12 @@
+export default {
+  template: `
+		<div class="mb-4">
+			<h1 class="text-3xl font-bold block pb-6"><slot></h1>
+			<nav class="flex space-x-4 block">
+				<a href="#">Home</a>
+				<a href="#">About</a>
+				<a href="#">Contact</a>
+			</nav>
+		</div>
+	`
+}
